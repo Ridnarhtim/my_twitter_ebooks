250fe3d50bd371fec76e77fc58f39e81f9797b0e
Moritz <moritz.schrenk@hotmail.com>
2018-12-19 12:08:21 +0000

    Some cleanup

--- seasons.rb
+++ seasons.rb
@@ -4,33 +4,37 @@ SeasonStruct = Struct.new(:folder, :message, :initial_chance, :include_in_replie
 
 class Seasons
 
-  attr_reader :seasons
+  def get(season_name)
+    case season_name
+    when "ValentinesDay"
+      SeasonStruct.new("Seasonal/ValentinesDay","Happy Valentine's Day",1,true)
+    when "Easter"
+      SeasonStruct.new("Seasonal/Easter","Happy Easter",1,true)
+    when "Halloween"
+      SeasonStruct.new("Seasonal/Halloween","Happy Halloween",1,true)
+    when "Navel"
+      SeasonStruct.new("Seasonal/Navel","Happy #いいおなかの日",1,true)
+    when "ChristmasCountdownMessage"
+      SeasonStruct.new("Seasonal/Christmas",christmas_timer_message,1,false)
+    when "ChristmasCountdownImages"
+      SeasonStruct.new(christmas_folder_chance,"",0.8,true)
+    when "ChristmasEve"
+      SeasonStruct.new("Seasonal/Christmas","It's Almost Christmas!",1,false)
+    when "Christmas"
+      SeasonStruct.new("Seasonal/Christmas","Merry Christmas",1,true)
+    when "Morning"
+      SeasonStruct.new("Seasonal/Morning",morning_message,1,true)
+    when "Default"
+      SeasonStruct.new("Bot","",0.8,false)
+    end
+  end 
 
-  def initialize
-    @seasons = {
-      "ValentinesDay" => SeasonStruct.new("Seasonal/ValentinesDay","Happy Valentine's Day",1,true),
-      "Easter" => SeasonStruct.new("Seasonal/Easter","Happy Easter",1,true),
-      "Halloween" => SeasonStruct.new("Seasonal/Halloween","Happy Halloween",1,true),
-      "Navel" => SeasonStruct.new("Seasonal/Navel","Happy #いいおなかの日",1,true),
-      "ChristmasCountdownMessage" => SeasonStruct.new("Seasonal/Christmas",christmas_timer_message,1,false),
-      "ChristmasCountdownImages" => SeasonStruct.new(christmas_folder_chance,"",0.8,true),
-      "ChristmasEver" => SeasonStruct.new("Seasonal/Christmas","It's Almost Christmas!",1,false),
-      "Christmas" => SeasonStruct.new("Seasonal/Christmas","Merry Christmas",1,true),
-      "Morning" => SeasonStruct.new("Seasonal/Morning",morning_message,1,true),
-      "Default" => SeasonStruct.new("Bot","",0.8,false)
-    }
-  end
-  
   def morning_message
     rand < 0.05 ? "Good morning degenerates" : "Good morning"
   end  
 
-  def get(season_name)
-    @seasons[season_name]
-  end 
-
   def days_to_christmas
-    (Date.parse("2018-12-24") - Date.today).to_i
+    (Date.parse("2018-12-25") - Date.today).to_i
   end
 
   def christmas_timer_message
